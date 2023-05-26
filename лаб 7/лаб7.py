3e0395d2c1d9643ab3dc82d9ce3812601aa604d8
Bawergan <103044373+Bawergan@users.noreply.github.com>
2023-05-26 16:46:38 +0300

    фв

--- лаб 7/лаб7.py
+++ лаб 7/лаб7.py
@@ -13,7 +13,7 @@ class client:
     
     def __EditBalance(self, sum):
         self.__balance += sum
-        print(f'{self.name} edited: {sum}\n')
+        print(f'{self.name} edited: {sum}')
     
     def Validator(self, sum):
         return (self.__balance + sum) >= 0        
@@ -31,34 +31,48 @@ class client:
         else: print(f'Transaction {sum} invalid')
 
 class transaction:
-    transactionSum = 0
-
     def __init__(self, transactionSum):
         self.transactionSum = transactionSum
     
     def Commision(self, comisionPersent):
+        print(f'commision: {self.transactionSum * comisionPersent/100}')
         return self.transactionSum * (1 - comisionPersent/100) 
 
 
+
+#создаем пользователя 1
 sergey = client(sername='Gavrilushkin', name='Sergey', age=10, balance=100)
 print(sergey.Info())
+print('-'*100)
 
+#изменяем баланс
 sergey.OperationManager(50)
 print('balance:', sergey.Info()['balance'])
 
 sergey.OperationManager(-25)
 print('balance:', sergey.Info()['balance'])
 
+print('-'*100)
+
+#создаем пользователя 2
 andrey = client(sername='', name='andrey', age=18, balance=100)
 
 print(sergey.Info())
 print(andrey.Info())
 
+print('-'*100)
+
+#делаем невалидный перевод (недостаточно средств)
+andrey.Transaction(clientToTransaction=sergey, sum=1000)
+
+print('-'*100)
 
 andrey.OperationManager(1000)
 
-andrey.Transaction(clientToTransaction=andrey, sum=1000)
+#делаем валидный перевод
+andrey.Transaction(clientToTransaction=sergey, sum=1000)
 
+print('-'*100)
 
 print(sergey.Info())
 print(andrey.Info())
